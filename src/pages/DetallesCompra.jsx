e986ba6bd6beff6a01feb1739eec472c43f7eabc
rodrigomzls <rrodrigomzls@gmail.com>
2025-05-26 23:20:43 -0500

    mas diseño

--- src/pages/DetallesCompra.jsx
+++ src/pages/DetallesCompra.jsx
@@ -125,14 +125,18 @@ const DetallesCompra = () => {
           return;
         }
         if (!detalles.material) {
-          alert(`Selecciona un material para el producto "${producto.nombre}".`);
+          alert(
+            `Selecciona un material para el producto "${producto.nombre}".`
+          );
           return;
         }
       }
 
       // Validar archivo (al menos 1 archivo)
       if (!detalles.imagenes || detalles.imagenes.length === 0) {
-        alert(`Sube al menos un archivo para el producto "${producto.nombre}".`);
+        alert(
+          `Sube al menos un archivo para el producto "${producto.nombre}".`
+        );
         return;
       }
     }
@@ -146,138 +150,147 @@ const DetallesCompra = () => {
 
   return (
     <div className="detalles-background">
-      <h2 className="titulo-compra">Detalles de la Compra</h2>
+      <div className="overlay-content">
+        <h2 className="titulo-compra">Detalles de la Compra</h2>
 
-      {productosSeleccionados.map((producto, index) => {
-        const detalles = detallesPorProducto[producto.id] || {
-          grupos: producto.cantidad > 1 ? [] : null,
-          color: "#ffffff",
-          material: "",
-          talla: "",
-          imagenes: [],
-        };
-        const grupos = detalles.grupos;
+        {productosSeleccionados.map((producto, index) => {
+          const detalles = detallesPorProducto[producto.id] || {
+            grupos: producto.cantidad > 1 ? [] : null,
+            color: "#ffffff",
+            material: "",
+            talla: "",
+            imagenes: [],
+          };
+          const grupos = detalles.grupos;
 
-        const esPoloPersonalizado = producto.nombre
-          .toLowerCase()
-          .includes("polo personalizado");
+          const esPoloPersonalizado = producto.nombre
+            .toLowerCase()
+            .includes("polo personalizado");
 
-        return (
-          <div key={index} className="producto-detalle">
-            <h3>{producto.nombre}</h3>
-            <p>
-              <strong>Cantidad:</strong> {producto.cantidad}
-            </p>
+          return (
+            <div key={index} className="producto-detalle">
+              <h3>{producto.nombre}</h3>
+              <p>
+                <strong>Cantidad:</strong> {producto.cantidad}
+              </p>
 
-            {producto.cantidad > 1 ? (
-              <div className="grupos-container">
-                <h4>Grupos</h4>
-                {grupos.map((grupo, idx) => (
-                  <div key={idx} className="grupo-item">
-                    <label>
-                      Talla:
-                      <input
-                        type="text"
-                        value={grupo.talla}
-                        onChange={(e) =>
-                          manejarCambioGrupo(
-                            producto.id,
-                            idx,
-                            "talla",
-                            e.target.value
-                          )
-                        }
-                      />
-                    </label>
-                    <label>
-                      Cantidad:
-                      <input
-                        type="number"
-                        min="1"
-                        max={
-                          producto.cantidad -
-                          obtenerCantidadTotalPorGrupos(grupos) +
-                          grupo.cantidad
-                        }
-                        value={grupo.cantidad}
-                        onChange={(e) =>
-                          manejarCambioGrupo(
-                            producto.id,
-                            idx,
-                            "cantidad",
-                            parseInt(e.target.value) || 1
-                          )
-                        }
-                      />
-                    </label>
-                    <button
-                      className="btn-delet-group"
-                      onClick={() => eliminarGrupo(producto.id, idx)}
-                    >
-                      X
-                    </button>
-                  </div>
-                ))}
+              {producto.cantidad > 1 ? (
+                <div className="grupos-container">
+                  <h4>Grupos</h4>
+                  {grupos.map((grupo, idx) => (
+                    <div key={idx} className="grupo-item">
+                      <label>
+                        Talla: 
+                        <input
+                          type="text"
+                          value={grupo.talla}
+                          onChange={(e) =>
+                            manejarCambioGrupo(
+                              producto.id,
+                              idx,
+                              "talla",
+                              e.target.value
+                            )
+                          }
+                        />
+                      </label>
+                      <label>
+                        Cantidad:
+                        <input
+                          type="number"
+                          min="1"
+                          max={
+                            producto.cantidad -
+                            obtenerCantidadTotalPorGrupos(grupos) +
+                            grupo.cantidad
+                          }
+                          value={grupo.cantidad}
+                          onChange={(e) =>
+                            manejarCambioGrupo(
+                              producto.id,
+                              idx,
+                              "cantidad",
+                              Math.max(1, parseInt(e.target.value) || 1)
+                            )
+                          }
+                        />
+                      </label>
+                      <button
+                        className="btn-delet-group"
+                        onClick={() => eliminarGrupo(producto.id, idx)}
+                      >
+                        X
+                      </button>
+                    </div>
+                  ))}
 
-                <button
-                  className="btn-add-group"
-                  onClick={() => agregarGrupo(producto.id)}
-                >
-                  Agregar Grupo
-                </button>
-              </div>
-            ) : (
-              <label>
-                Talla:
-                <input
-                  type="text"
-                  value={detalles.talla}
-                  onChange={(e) =>
-                    manejarCambioCampoSimple(producto.id, "talla", e.target.value)
-                  }
-                />
-              </label>
-            )}
-
-            {/* Campos adicionales para polo personalizado */}
-            {esPoloPersonalizado && (
-              <>
+                  <button
+                    className="btn-add-group"
+                    onClick={() => agregarGrupo(producto.id)}
+                  >
+                    Agregar Grupo
+                  </button>
+                </div>
+              ) : (
                 <label>
-                  Color:
-                  <input
-                    type="color"
-                    value={detalles.color}
-                    onChange={(e) => manejarColor(producto.id, e.target.value)}
-                  />
-                </label>
-                <label>
-                  Material:
+                  Talla:
                   <input
                     type="text"
-                    value={detalles.material}
-                    onChange={(e) => manejarMaterial(producto.id, e.target.value)}
-                    placeholder="Material (ej. algodón)"
+                    value={detalles.talla}
+                    onChange={(e) =>
+                      manejarCambioCampoSimple(
+                        producto.id,
+                        "talla",
+                        e.target.value
+                      )
+                    }
                   />
                 </label>
-              </>
-            )}
+              )}
 
-            <label>
-              Archivo{producto.cantidad > 1 ? "s" : ""}:
-              <input
-                type="file"
-                accept="image/*"
-                multiple
-                onChange={(e) => manejarImagenes(producto.id, e)}
-              />
-            </label>
-          </div>
-        );
-      })}
+              {esPoloPersonalizado && (
+                <>
+                  <label>
+                    Color:
+                    <input
+                      type="color"
+                      value={detalles.color}
+                      onChange={(e) =>
+                        manejarColor(producto.id, e.target.value)
+                      }
+                    />
+                  </label>
+                  <label>
+                    Material:
+                    <input
+                      type="text"
+                      value={detalles.material}
+                      onChange={(e) =>
+                        manejarMaterial(producto.id, e.target.value)
+                      }
+                      placeholder="Material (ej. algodón)"
+                    />
+                  </label>
+                </>
+              )}
 
-      <button className="btn-add-group" onClick={continuar}>
-        Continuar con el Pago
-      </button>
+              <label>
+                Archivo{producto.cantidad > 1 ? "s" : ""}:
+                <input
+                  type="file"
+                  accept="image/*"
+                  multiple
+                  onChange={(e) => manejarImagenes(producto.id, e)}
+                />
+              </label>
+            </div>
+          );
+        })}
+
+        <button className="btn-add-group" onClick={continuar}>
+          Continuar con el Pago
+        </button>
+      </div>
     </div>
   );
 };
